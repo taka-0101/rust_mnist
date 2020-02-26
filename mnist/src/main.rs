23ee0f806c5881afc3d4e71b8d719d83fa6e53a4
taka-0101 <rakunou.jr0120@gmail.com>
2020-02-27 00:08:48 +0900

    バッジ化のための実装とクロスエントロピー誤差を求める関数の作成、イメージ読み込みの作成

--- mnist/src/main.rs
+++ mnist/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 extern crate image;
 use rand::Rng;
+use std::fs;
 use image::GenericImageView;
 use image::DynamicImage;
 use image::Rgba;
@@ -11,10 +12,10 @@ struct Layer {
 }
 
 impl Layer {
-    fn new(p: u16, n: u16) -> Layer {
+    fn new(p: u16, n: u16, b:u16) -> Layer {
         Layer{
             weights: Layer::weights_init(p, n),
-            biases: Layer::biases_init(n)
+            biases: Layer::biases_init(n, b)
         }
         
         
@@ -33,14 +34,16 @@ impl Layer {
         weights
     }
 
-    fn biases_init(n: u16) -> Vec<Vec<f64>> {
+    fn biases_init(n: u16, b:u16) -> Vec<Vec<f64>> {
         let mut biases = Vec::new();
-        let mut b = Vec::new();
         let mut rng = rand::thread_rng();
-        for _i in 0..n {
-            b.push(rng.gen::<f64>());
+        for _j in 0..b{
+            let mut b = Vec::new();
+            for _i in 0..n {
+                b.push(rng.gen::<f64>());
+            }
+            biases.push(b);
         }
-        biases.push(b);
         biases
     }
 }
@@ -71,6 +74,7 @@ fn vector_dot(v1: Vec<Vec<f64>>, v2: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
             }
             result.push(v)
         }
+        println!("ERROR: Not much vector size")
     }
     else{
         println!("ERROR: Not much vector size")
@@ -110,44 +114,103 @@ fn sigmoid(x: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
     result
 }
 
-fn identity_function(x: Vec<Vec<f64>>) -> Vec<Vec<f64>>{
-    x
+fn cross_entropy_error(y:Vec<Vec<f64>>, t:Vec<Vec<f64>>) -> Vec<f64>{
+    let delta = 1e-7;
+    let mut result = Vec::new();
+    let one = 1.0_f64;
+    let e = one.exp();
+    if y.len() == t.len(){
+        for i in 0..y.len(){
+            let mut r = 0.0 as f64;
+            for j in 0..y[i].len(){
+                let value = y[i][j] + delta;
+                r = r + (t[i][j] * value.log(e));
+            }
+            result.push(r*-1.0);
+        }
+    }
+    else{
+        println!("ERROR:Vector not mutch size, cross_entropy_error");
+    }
+    result
 }
 
-fn load_image(path: &str) -> Vec<Vec<f64>>{
+fn identity_function(x: Vec<Vec<f64>>) -> Vec<Vec<f64>>{
+    let mut result = Vec::new();
+    for i in 0..x.len(){
+        let mut value = 0.0 as f64;
+        let mut r = Vec::new();
+        for j in 0..x[i].len(){
+            value = value + x[i][j];
+        }
+        for j in 0..x[i].len(){
+            r.push(x[i][j] / value);
+        }
+        result.push(r);
+    }
+    result
+}
+
+fn load_image(path: &str) -> Vec<f64>{
     let img: DynamicImage = image::open(path).unwrap();
     let (width, height) = img.dimensions();
 
     let mut result = Vec::new();
-    let mut r = Vec::new();
     for y in 0..height {
         for x in 0..width {
             let pixel: Rgba<u8> = img.get_pixel(x, y);
             if pixel[0] > 0 {
-                r.push(1.0 as f64);
+                result.push(1.0 as f64);
             }
             else {
-                r.push(0.0 as f64);
+                result.push(0.0 as f64);
             }
         }
     }
-    result.push(r);
+    result
+}
+
+
+fn read_file(path: &str) -> Vec<Vec<f64>> {
+    let paths = fs::read_dir(path).unwrap();
+    let mut result = Vec::new();
+    for (i, p) in paths.enumerate() {
+        //println!("Name: {}", p.unwrap().path().display())
+        let p = p.unwrap().path();
+        let p_ = p.to_string_lossy().to_string();
+        result.push(load_image(&p_));
+        if i == 99{
+            break;
+        }
+    }
     result
 }
 
 fn main() {
-    let ly1 = Layer::new(784, 50);
-    let ly2 = Layer::new(50, 50);
-    let ly3 = Layer::new(50, 2);
+    let ly1 = Layer::new(784, 50, 100);
+    let ly2 = Layer::new(50, 50, 100);
+    let ly3 = Layer::new(50, 10, 100);
     
     let n: Vec<f64> = vec![2.5, 3.0];
     let mut neu = Vec::new();
     neu.push(n);
+    let neu = read_file("C:/Users/rakun/Documents/mnist_png/mnist_png/testing/0/");
+    
+    let mut label = Vec::new();
+    
+    for _i in 0..100 {
+        let mut kari = Vec::new();
+        kari.push(1.0 as f64);
+        for _i in 0..9 {
+            kari.push(0.0 as f64)
+        }
+        label.push(kari)
+    }
 
-    let r1 = calc_neuron(load_image("mnist_png/testing/0/3.png"), ly1);
+    let r1 = calc_neuron(neu, ly1);
     let r2 = calc_neuron(sigmoid(r1), ly2);
     let r3 = calc_neuron(sigmoid(r2), ly3);
-
-    println!("{:?}", identity_function(r3));
+    let result = cross_entropy_error(identity_function(r3), label);
+    println!("{:?}", result);
 
 }
